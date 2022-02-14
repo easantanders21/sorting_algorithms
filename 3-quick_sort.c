17a193b40fa169800da78899a8ce1400e04e5ed5
easantanders21 <3823@holbertonschool.com>
2022-02-14 22:14:58 +0000

    task 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,29 +1,80 @@
+#include "sort.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "sort.h"
-
-void swap(int *xp, int *yp)
+/**
+ *swap2 - des
+ *@array: array
+ *@x: array element
+ *@y: array element
+ */
+void swap2(int *array, ssize_t x, ssize_t y)
 {
-	int temp = *xp;
-	*xp = *yp;
-	*yp = temp;
+	int tmp;
+
+	tmp = array[x];
+	array[x] = array[y];
+	array[y] = tmp;
 }
-
-void lomuto_partition(int *array, int lim_left, int lim_right, size_t size)
+/**
+ *lomuto_partition - des
+ *@array: array
+ *@first: first array element
+ *@last: last array element
+ *@size: size array
+ *Return: return the position of the last element sorted
+ */
+int lomuto_partition(int *array, ssize_t first, ssize_t last, size_t size)
 {
-	int pivot = lim_right, min = lim_left, max = lim_left;
+	int pivot = array[last];
+	ssize_t current = first, finder;
 
-	while ()
+	for (finder = first; finder < last; finder++)
 	{
-		
+		if (array[finder] < pivot)
+		{
+			if (array[current] != array[finder])
+			{
+				swap2(array, current, finder);
+				print_array(array, size);
+			}
+			current++;
+		}
+	}
+	if (array[current] != array[last])
+	{
+		swap2(array, current, last);
+		print_array(array, size);
+	}
+	return (current);
+}
+/**
+ *qs - des
+ *@array: array
+ *@lim_left: first array element
+ *@lim_right: last array element
+ *@size: array size
+ */
+void qs(int *array, ssize_t lim_left, ssize_t lim_right, int size)
+{
+	ssize_t pivot = 0;
+
+
+	if (lim_left < lim_right)
+	{
+		pivot = lomuto_partition(array, lim_left, lim_right, size);
+
+		qs(array, lim_left, pivot - 1, size);
+		qs(array, pivot + 1, lim_right, size);
 	}
 }
-
-void qs(int*array, int lim_left, int lim_right, size_t size)
-{
-	int index_pivot = lomuto_partition(array, lim_left, lim_right, size);
-}
+/**
+ *quick_sort - des
+ *@array: array
+ *@size: array size
+ */
 void quick_sort(int *array, size_t size)
 {
+	if (!array || size < 2)
+		return;
 	qs(array, 0, size - 1, size);
 }
